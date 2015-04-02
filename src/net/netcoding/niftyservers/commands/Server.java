2e8ad2eb273192b3c2f9c5ae58ec9fd3ecb3cc91
Brian Graham <support@netcoding.net>
2015-04-02 15:14:57 -0400

    Removed /nreload in favor of automatic reloading Removed test output of /servers

--- src/net/netcoding/niftyservers/commands/Server.java
+++ src/net/netcoding/niftyservers/commands/Server.java
@@ -1,11 +1,14 @@
 package net.netcoding.niftyservers.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
 import net.netcoding.niftybukkit.mojang.MojangProfile;
 import net.netcoding.niftybukkit.mojang.exceptions.ProfileNotFoundException;
 import net.netcoding.niftybukkit.util.ListUtil;
-import net.netcoding.niftyservers.cache.Cache;
+import net.netcoding.niftyservers.NiftyServers;
 import net.netcoding.niftyservers.cache.ServerInfo;
 import net.netcoding.niftyservers.listeners.ServerInventory;
 
@@ -16,44 +19,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Server extends BukkitCommand {
 
 	public Server(JavaPlugin plugin) {
-		super(plugin, "server");
+		super(plugin, "servers");
 		this.setMinimumArgsLength(0);
 		this.setCheckPerms(false);
 		this.setBungeeOnly();
 	}
 
-	/*@Override
-	public List<String> onTabComplete(CommandSender sender, String label, String[] args) {
-		List<String> serverNames = ServerInventory.getServerNames(this, sender);
-
-		if (ListUtil.isEmpty(args))
-			return serverNames;
-		else {
-			List<String> exactNames = new ArrayList<>();
-			List<String> matchedNames = new ArrayList<>();
-
-			for (String serverName : serverNames) {
-				String checkName = serverName.toLowerCase();
-
-				if (checkName.equalsIgnoreCase(args[0]))
-					exactNames.add(serverName);
-				else if (checkName.startsWith(args[0].toLowerCase()))
-					matchedNames.add(serverName);
-			}
-
-			exactNames.addAll(matchedNames);
-			return exactNames;
-		}
-	}*/
-
 	@Override
 	public void onCommand(CommandSender sender, String alias, String[] args) throws Exception {
 		String action = isConsole(sender) ? "list" : (ListUtil.isEmpty(args) ? "chest" : args[0]);
 		MojangProfile profile = null;
-		System.out.println("Test x1");
 
 		if (isPlayer(sender)) {
-			System.out.println("Test x2");
 			try {
 				profile = NiftyBukkit.getMojangRepository().searchByPlayer((Player)sender);
 			} catch (ProfileNotFoundException pnfe) {
@@ -63,22 +40,18 @@ public class Server extends BukkitCommand {
 		}
 
 		if (action.matches("^list|chest$")) {
-			System.out.println("Test x3");
 			if (action.equalsIgnoreCase("chest")) {
-				System.out.println("Test x4");
 				if (this.hasPermissions(sender, action)) {
-					System.out.println("Test x5");
 					ServerInventory.processChest(profile);
 					return;
 				}
 			}
 
-			System.out.println("Test x6");
 			ServerInventory.showList(sender);
 		} else {
 			if (args.length == 1) {
 				String serverName = action.replace("\"", "");
-				ServerInfo serverInfo = Cache.Servers.getServer(serverName);
+				ServerInfo serverInfo = NiftyServers.getServersConfig().getServer(serverName);
 
 				if (serverInfo != null) {
 					if (this.hasPermissions(sender, "server", serverName))
@@ -88,6 +61,29 @@ public class Server extends BukkitCommand {
 			} else
 				this.showUsage(sender);
 		}
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, String label, String[] args) {
+		List<String> serverNames = ServerInventory.getServerNames(sender);
+
+		if (ListUtil.isEmpty(args))
+			return serverNames;
+
+		List<String> exactNames = new ArrayList<>();
+		List<String> matchedNames = new ArrayList<>();
+
+		for (String serverName : serverNames) {
+			String checkName = serverName.toLowerCase();
+
+			if (checkName.equalsIgnoreCase(args[0]))
+				exactNames.add(serverName);
+			else if (checkName.startsWith(args[0].toLowerCase()))
+				matchedNames.add(serverName);
+		}
+
+		exactNames.addAll(matchedNames);
+		return exactNames;
 	}
 
 }
